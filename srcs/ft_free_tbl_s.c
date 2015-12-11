d829a75cee1cb3c73a05324c934466147233c527
dolewski <dolewski@student.42.fr>
2015-12-11 11:14:24 +0100

    add ft_free_tbl_s

--- srcs/ft_free_tbl_s.c
+++ srcs/ft_free_tbl_s.c
@@ -1,28 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*   ft_free_tbl_s.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dolewski <dolewski@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/11/23 14:58:26 by dolewski          #+#    #+#             */
-/*   Updated: 2015/12/11 11:01:16 by dolewski         ###   ########.fr       */
+/*   Created: 2015/12/09 17:37:18 by dolewski          #+#    #+#             */
+/*   Updated: 2015/12/09 17:37:22 by dolewski         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memset(void *b, int c, size_t len)
+void	ft_free_tbl_s(char **tbl)
 {
-	unsigned char	*pb;
-	size_t			i;
+	int		i;
 
 	i = 0;
-	pb = (unsigned char *)b;
-	while (b != NULL && i < len)
+	while (tbl[i] != NULL)
 	{
-		pb[i] = (unsigned char)c;
+		free(tbl[i]);
 		i++;
 	}
-	return (pb);
+	free(tbl);
 }
