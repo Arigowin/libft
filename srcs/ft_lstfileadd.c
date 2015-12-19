00ba555fea8100c82ff263acfada5fda6c1079f6
David OLEWSKI <dolewski@e3r12p15.42.fr>
2015-12-19 19:39:57 +0100

    correction bug mineur

--- srcs/ft_lstfileadd.c
+++ srcs/ft_lstfileadd.c
@@ -6,22 +6,18 @@
 /*   By: dolewski <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/18 14:17:38 by dolewski          #+#    #+#             */
-/*   Updated: 2015/12/19 14:03:06 by dolewski         ###   ########.fr       */
+/*   Updated: 2015/12/19 19:04:42 by dolewski         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-#include <stdio.h>
-
 void				ft_lstfileadd(t_file **lstfile, t_file *new)
 {
-	if (lstfile)
-	{
-		if (*lstfile && new)
-		{
-			new->next = (*lstfile);
-			*lstfile = new;
-		}
-	}
+	t_list *tmp;
+
+	tmp = *lstfile;
+	while (tmp->next)
+		tmp = tmp->next;
+	tmp->next = new;
 }
--- srcs/ft_lstfilenew.c
+++ srcs/ft_lstfilenew.c
@@ -6,14 +6,12 @@
 /*   By: dolewski <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/18 14:17:33 by dolewski          #+#    #+#             */
-/*   Updated: 2015/12/19 14:10:24 by dolewski         ###   ########.fr       */
+/*   Updated: 2015/12/19 19:36:59 by dolewski         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "libft.h"
-
-#include <stdio.h>
 
 t_file				*ft_lstfilenew(char *data, int fd)
 {
@@ -22,14 +20,13 @@ t_file				*ft_lstfilenew(char *data, int fd)
 	new = (t_file *)malloc(sizeof(t_file));
 	if (new != NULL)
 	{
-		printf("ddd\n");
-		printf("%s\n", data);
 		if (data == NULL)
 			new->data = NULL;
 		else
+		{
+			new->data = ft_strnew(ft_strlen(data));
 			ft_memcpy(new->data, data, ft_strlen(data));
-		printf("ddd\n");
-		printf("%s\n", new->data);
+		}
 		new->fd = fd;
 	}
 	return (new);
