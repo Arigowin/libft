a14d96eebf59d6428dc5d22c51df89d1dd1ba765
dolewski <dolewski@student.42.fr>
2016-01-25 16:30:20 +0100

    proper

--- srcs/ft_strproperjoin.c
+++ srcs/ft_strproperjoin.c
@@ -6,28 +6,19 @@
 /*   By: dolewski <dolewski@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/13 11:59:58 by dolewski          #+#    #+#             */
-/*   Updated: 2016/01/13 12:04:53 by dolewski         ###   ########.fr       */
+/*   Updated: 2016/01/25 16:30:10 by dolewski         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
-char	*ft_strproperjoin(char *s1, char *s2)
+void	ft_strproperjoin(char *s1, char *s2)
 {
 	char *tmp;
 
-	if (!s1 && !s2)
-		return (NULL);
-	if (!s1)
-		tmp = ft_strdup(s2);
-	else if (!s2)
-		tmp = ft_strdup(s1);
-	else
-	{
-		tmp = ft_strjoin(s1, s2);
-		ft_strdel(&s1);
-		ft_strdel(&s2);
-	}
-	return (tmp);
+	tmp = ft_strjoin(s1, s2);
+	ft_strdel(&s1);
+	s1 = ft_strdup(tmp);
+	ft_strdel(tmp);
 }
